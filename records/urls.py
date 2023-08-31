c0f28936c9437bd20b7904364ebb7086f34b579f
Onkur Lal <onkur24lal@gmail.com>
2023-08-31 13:50:01 -0400

    added create and edit for personal details

--- records/urls.py
+++ records/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from records.views import my_record, home
+from records.views import my_record, home, create_details, update_details
 
 urlpatterns =[
     path('', home, name='home'),
     path('my_record/', my_record, name='my_record'),
+    path('details/create', create_details, name='create_details'),
+    path('details/<int:id>/update', update_details, name='update_details'),
 ]
--- records/views.py
+++ records/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, render
+from django.shortcuts import render, get_object_or_404, redirect
 from records.models import PersonalDetail, Disease, Doctor, Pharmacy, Medication
 from django.contrib.auth.decorators import login_required
 from records.forms import PersonalDetailForm, DiseaseForm, DoctorForm, PharmacyForm, MedicationForm
@@ -9,19 +9,45 @@ def home(request):
 
 @login_required
 def my_record(request):
-    medications = get_object_or_404(Medication, patient = request.user)
-    details = get_object_or_404(PersonalDetail, patient = request.user)
-    doctors = get_object_or_404(Doctor, patient = request)
-    diseases = get_object_or_404(Disease, patient = request)
-    pharmacies = get_object_or_404(Pharmacy, patient = request)
+    medications = Medication.objects.filter(patient=request.user)
+    doctors = Doctor.objects.filter(patient=request.user)
+    diseases = Disease.objects.filter(patient=request.user)
+    pharmacies = Pharmacy.objects.filter(patient=request.user)
     context = {
         'medications': medications,
-        'details': details,
         'doctors': doctors,
         'diseases': diseases,
         'pharmacies': pharmacies,
     }
     return render(request, 'records/my_record.html', context)
 
-# @login_required
-# def create_details(request):
+@login_required
+def create_details(request):
+    if request.method == 'POST':
+        form = PersonalDetailForm(request.POST)
+        if form.is_valid():
+            details = form.save(False)
+            details.patient = request.user
+            details.save()
+            return redirect('my_record')
+    else:
+        form = PersonalDetailForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'details/create.html', context)
+
+@login_required
+def update_details(request, id):
+    details = get_object_or_404(PersonalDetail, id=id)
+    if request.method == 'POST':
+        form = PersonalDetailForm(request.POST, instance=details)
+        if form.is_valid():
+            form.save()
+            return redirect('my_record')
+    else:
+        form = PersonalDetailForm(instance=details)
+    context = {
+        'form': form,
+    }
+    return render(request, 'details/update.html', context)
