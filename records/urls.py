4289078fde0203149b87ff3096419bcbed932ee5
Onkur Lal <onkur24lal@gmail.com>
2023-08-31 14:33:08 -0400

    added create, update, delete for docotor

--- records/urls.py
+++ records/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from records.views import my_record, home, create_details, update_details, create_disease, update_disease,delete_disease
+from records.views import my_record, home, create_details, update_details, create_disease, update_disease,delete_disease, create_doctor, update_doctor, delete_doctor
 
 urlpatterns =[
     path('', home, name='home'),
@@ -9,4 +9,7 @@ urlpatterns =[
     path('diseases/create', create_disease, name='create_disease'),
     path('diseases/<int:id>/update', update_disease, name='update_disease'),
     path('diseases/<int:id>/delete', delete_disease, name='delete_disease'),
+    path('doctors/create', create_doctor, name='create_doctor'),
+    path('doctors/<int:id>/delete', delete_doctor, name='delete_doctor'),
+    path('doctors/<int:id>/update', update_doctor, name='update_doctor'),
 ]
--- records/views.py
+++ records/views.py
@@ -92,3 +92,44 @@ def delete_disease(request, id):
         'disease': disease,
     }
     return render(request, 'diseases/delete.html',context)
+
+@login_required
+def create_doctor(request):
+    if request.method == 'POST':
+        form = DoctorForm(request.POST)
+        if form.is_valid():
+            doctor = form.save(False)
+            doctor.patient = request.user
+            doctor.save()
+            return redirect('my_record')
+    else:
+        form = DoctorForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'doctors/create.html', context)
+
+@login_required
+def update_doctor(request, id):
+    doctor = get_object_or_404(Doctor, id=id)
+    if request.method == 'POST':
+        form = DoctorForm(request.POST, instance=doctor)
+        if form.is_valid():
+            form.save()
+            return redirect('my_record')
+    else:
+        form = DoctorForm(instance=doctor)
+    context = {
+        'form': form,
+    }
+    return render(request, 'doctors/update.html', context)
+
+def delete_doctor(request, id):
+    doctor = get_object_or_404(Doctor, id=id)
+    if request.method == 'POST':
+        doctor.delete()
+        return redirect('my_record')
+    context = {
+        'doctor': doctor,
+    }
+    return render(request, 'doctors/delete.html',context)
