dd4444bb24e3b4e126c9aee0cd79410cda9aedf9
Onkur Lal <onkur24lal@gmail.com>
2023-08-31 12:58:59 -0400

    added user signup and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from accounts.views import user_login
+from accounts.views import user_login, user_signup
 
 urlpatterns = [
     path('login/',user_login, name='user_login'),
+    path('signup/',user_signup, name='user_signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignupForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 # Create your views here.
@@ -27,3 +27,33 @@ def user_login(request):
         'form': form,
     }
     return render(request, 'accounts/login.html', context)
+
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            password = form.cleaned_data['password']
+            password_confirmation = form.cleaned_data['password_confirmation']
+
+            if password == password_confirmation:
+                user = User.objects.create_user(
+                    username=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    password=password
+                )
+            login(request, user)
+            return redirect('my_record')
+    else:
+        form = SignupForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
