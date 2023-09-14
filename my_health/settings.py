571a98e9399d57141f15181bb1de95ca30db97a8
Onkur Lal <onkur24lal@gmail.com>
2023-09-14 17:13:20 -0400

    deploy to render

--- my_health/settings.py
+++ my_health/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8%uy0dw)tnyz(g&p!u!0ccp(!b*-2(&q%gwszjez@zu3d=#stb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
